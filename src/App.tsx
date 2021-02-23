1964aeee417fc7f4883de96156c696c3116a8b81
jafidiesel <jafibravin@outlook.com>
2021-02-23 20:57:06 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
+import Body from './components/Body/Body';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Body />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Body() {
+  return (
+    <p>body</p>
+  );
+}
+
+export default Body;
